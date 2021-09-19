de90214b38e4ad546e1d0357d7f2e970a5f81098
Velinova <viktorijavelinovaa@gmail.com>
2021-09-19 17:42:57 +0200

    Added audio player and learning component is finished.

--- src/app/learn/learn.component.ts
+++ src/app/learn/learn.component.ts
@@ -1,9 +1,88 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
+import { StreamState } from "../interfaces";
+import { AudioService } from "../services/audio.service";
+import { LetterService } from "../services/letter.service";
+import { SoundsService } from "../services/sounds.service";
 
 @Component({
   templateUrl: './learn.component.html',
   styleUrls: ['./learn.component.scss']
 })
-export class LearnComponent {
+export class LearnComponent implements OnInit {
+  word: string;
+  letter: string;
+  letters: any[];
+  currentLetter: any;
+  letterIndex: number;
 
+  // #region Player
+
+  state: StreamState;
+  files: Array<any> = [];
+
+  // #endregion Player
+
+  constructor(
+    private audioService: AudioService,
+    private soundService: SoundsService,
+    private letterService: LetterService
+  ) {
+    this.state = {
+      playing: false,
+      readableCurrentTime: '',
+      readableDuration: '',
+      duration: undefined,
+      currentTime: undefined,
+      canplay: false,
+      error: false
+    };
+    this.word = '';
+    this.letter = '';
+    this.letters = [];
+    this.letterIndex = 0;
+  }
+
+  ngOnInit(): void {
+    this.letters = this.letterService.getLetters();
+    this.setLetter();
+
+    this.soundService.getFiles().subscribe(files => {
+      this.files = files;
+    });
+
+    this.audioService.getState().subscribe(state => {
+      this.state = state;
+    })
+  }
+
+  next(): void {
+    this.letterIndex++;
+    this.setLetter();
+  }
+
+  previous(): void {
+    this.letterIndex--;
+    this.setLetter();
+  }
+
+  setLetter(): void {
+    this.currentLetter = {
+      Id: this.letters[this.letterIndex].Id,
+      Letter: this.letters[this.letterIndex].Letter,
+      Image: this.letterService.getLetterImage(this.letters[this.letterIndex].Letter)
+    };
+
+    this.word = `${this.currentLetter.Image[0].toUpperCase()}${this.currentLetter.Image.slice(1, this.currentLetter.Image.length).replace('.png', '')}`;
+    this.letter = `${this.currentLetter.Image[0].toUpperCase()}${this.currentLetter.Image.slice(0, 1)}`;
+  }
+
+  listen(): void {
+    let letterSound = this.files.filter((file) => { return file.text === this.currentLetter.Letter; })[0];
+    this.audioService.playStream(letterSound.url).subscribe(() => { });
+    
+    setTimeout(() => {
+      let wordSound = this.files.filter((file) => { return file.text[0].toLowerCase() === this.currentLetter.Letter.toLowerCase() && file.text.length > 1; })[0];
+      this.audioService.playStream(wordSound.url).subscribe(() => { });
+    }, 1000)
+  }
 }
---
+++ src/app/services/sounds.service.ts
@@ -0,0 +1,324 @@
+import { Injectable } from "@angular/core";
+import { of } from "rxjs";
+
+@Injectable({
+  providedIn: "root"
+})
+export class SoundsService {
+  files: any = [
+    {
+      url: "assets/sounds/letters/А.mp3",
+      text: "А",
+      isLetter: true
+    },
+    {
+      url:"assets/sounds/letters/Б.mp3",
+      text: "Б",
+      isLetter: true
+    },
+    {
+      url: "assets/sounds/letters/В.mp3",
+      text: "В",
+      isLetter: true
+    },
+    {
+      url: "assets/sounds/letters/Г.mp3",
+      text: "Г",
+      isLetter: true
+    },
+    {
+      url: "assets/sounds/letters/Д.mp3",
+      text: "Д",
+      isLetter: true
+    },
+    {
+      url: "assets/sounds/letters/Ѓ.mp3",
+      text: "Ѓ",
+      isLetter: true
+    },
+    {
+      url: "assets/sounds/letters/Е.mp3",
+      text: "Е",
+      isLetter: true
+    },
+    {
+      url: "assets/sounds/letters/Ж.mp3",
+      text: "Ж",
+      isLetter: true
+    },
+    {
+      url: "assets/sounds/letters/З.mp3",
+      text: "З",
+      isLetter: true
+    },
+    {
+      url: "assets/sounds/letters/Ѕ.mp3",
+      text: "Ѕ",
+      isLetter: true
+    },
+    {
+      url: "assets/sounds/letters/И.mp3",
+      text: "И",
+      isLetter: true
+    },
+    {
+      url: "assets/sounds/letters/Ј.mp3",
+      text: "Ј",
+      isLetter: true
+    },
+    {
+      url: "assets/sounds/letters/К.mp3",
+      text: "К",
+      isLetter: true
+    },
+    {
+      url: "assets/sounds/letters/Л.mp3",
+      text: "Л",
+      isLetter: true
+    },
+    {
+      url: "assets/sounds/letters/Љ.mp3",
+      text: "Љ",
+      isLetter: true
+    },
+    {
+      url: "assets/sounds/letters/М.mp3",
+      text: "М",
+      isLetter: true
+    },
+    {
+      url: "assets/sounds/letters/Н.mp3",
+      text: "Н",
+      isLetter: true
+    },
+    {
+      url: "assets/sounds/letters/Њ.mp3",
+      text: "Њ",
+      isLetter: true
+    },
+    {
+      url: "assets/sounds/letters/О.mp3",
+      text: "О",
+      isLetter: true
+    },
+    {
+      url: "assets/sounds/letters/П.mp3",
+      text: "П",
+      isLetter: true
+    },
+    {
+      url: "assets/sounds/letters/Р.mp3",
+      text: "Р",
+      isLetter: true
+    },
+    {
+      url: "assets/sounds/letters/С.mp3",
+      text: "С",
+      isLetter: true
+    },
+    {
+      url: "assets/sounds/letters/Т.mp3",
+      text: "Т",
+      isLetter: true
+    },
+    {
+      url: "assets/sounds/letters/Ќ.mp3",
+      text: "Ќ",
+      isLetter: true
+    },
+    {
+      url: "assets/sounds/letters/У.mp3",
+      text: "У",
+      isLetter: true
+    },
+    {
+      url: "assets/sounds/letters/Ф.mp3",
+      text: "Ф",
+      isLetter: true
+    },
+    {
+      url: "assets/sounds/letters/Х.mp3",
+      text: "Х",
+      isLetter: true
+    },
+    {
+      url: "assets/sounds/letters/Ц.mp3",
+      text: "Ц",
+      isLetter: true
+    },
+    {
+      url: "assets/sounds/letters/Ч.mp3",
+      text: "Ч",
+      isLetter: true
+    },
+    {
+      url: "assets/sounds/letters/Џ.mp3",
+      text: "Џ",
+      isLetter: true
+    },
+    {
+      url: "assets/sounds/letters/Ш.mp3",
+      text: "Ш",
+      isLetter: true
+    },
+    {
+      url: "assets/sounds/words/авион.mp3",
+      text: "авион",
+      isLetter: false
+    },
+    {
+      url: "assets/sounds/words/балон.mp3",
+      text: "балон",
+      isLetter: false
+    },
+    {
+      url: "assets/sounds/words/виножито.mp3",
+      text: "виножито",
+      isLetter: false
+    },
+    {
+      url: "assets/sounds/words/готвач.mp3",
+      text: "готвач",
+      isLetter: false
+    },
+    {
+      url: "assets/sounds/words/диносаурус.mp3",
+      text: "диносаурус",
+      isLetter: false
+    },
+    {
+      url: "assets/sounds/words/ѓеврек.mp3",
+      text: "ѓеврек",
+      isLetter: false
+    },
+    {
+      url: "assets/sounds/words/елка.mp3",
+      text: "елка",
+      isLetter: false
+    },
+    {
+      url: "assets/sounds/words/жирафа.mp3",
+      text: "жирафа",
+      isLetter: false
+    },
+    {
+      url: "assets/sounds/words/зајче.mp3",
+      text: "зајче",
+      isLetter: false
+    },
+    {
+      url: "assets/sounds/words/ѕвонче.mp3",
+      text: "ѕвонче",
+      isLetter: false
+    },
+    {
+      url: "assets/sounds/words/игла.mp3",
+      text: "игла",
+      isLetter: false
+    },
+    {
+      url: "assets/sounds/words/јајце.mp3",
+      text: "јајце",
+      isLetter: false
+    },
+    {
+      url: "assets/sounds/words/куќа.mp3",
+      text: "куќа",
+      isLetter: false
+    },
+    {
+      url: "assets/sounds/words/лижавче.mp3",
+      text: "лижавче",
+      isLetter: false
+    },
+    {
+      url: "assets/sounds/words/љубов.mp3",
+      text: "љубов",
+      isLetter: false
+    },
+    {
+      url: "assets/sounds/words/маче.mp3",
+      text: "маче",
+      isLetter: false
+    },
+    {
+      url: "assets/sounds/words/носорог.mp3",
+      text: "носорог",
+      isLetter: false
+    },
+    {
+      url: "assets/sounds/words/њујорк.mp3",
+      text: "њујорк",
+      isLetter: false
+    },
+    {
+      url: "assets/sounds/words/оган.mp3",
+      text: "оган",
+      isLetter: false
+    },
+    {
+      url: "assets/sounds/words/паун.mp3",
+      text: "паун",
+      isLetter: false
+    },
+    {
+      url: "assets/sounds/words/радио.mp3",
+      text: "радио",
+      isLetter: false
+    },
+    {
+      url: "assets/sounds/words/сонце.mp3",
+      text: "сонце",
+      isLetter: false
+    },
+    {
+      url: "assets/sounds/words/торта.mp3",
+      text: "торта",
+      isLetter: false
+    },
+    {
+      url: "assets/sounds/words/ќуп.mp3",
+      text: "ќуп",
+      isLetter: false
+    },
+    {
+      url: "assets/sounds/words/уста.mp3",
+      text: "уста",
+      isLetter: false
+    },
+    {
+      url: "assets/sounds/words/фонтана.mp3",
+      text: "фонтана",
+      isLetter: false
+    },
+    {
+      url: "assets/sounds/words/хеликоптер.mp3",
+      text: "хеликоптер",
+      isLetter: false
+    },
+    {
+      url: "assets/sounds/words/цвекло.mp3",
+      text: "цвекло",
+      isLetter: false
+    },
+    {
+      url: "assets/sounds/words/четка.mp3",
+      text: "четка",
+      isLetter: false
+    },
+    {
+      url: "assets/sounds/words/џамлии.mp3",
+      text: "џамлии",
+      isLetter: false
+    },
+    {
+      url: "assets/sounds/words/шах.mp3",
+      text: "шах",
+      isLetter: false
+    }
+  ];
+
+  getFiles() {
+    return of(this.files);
+  }
+}
